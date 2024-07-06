779a9c299a8c02652a41cacdb2169806e60c1fde
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-07-06 17:44:18 +0530

    Using the Prompt method for taking user input

--- JavaScript NoteBook/CleverProgramming/BasicLecture /Basic.js
+++ JavaScript NoteBook/CleverProgramming/BasicLecture /Basic.js
@@ -1,4 +1,11 @@
 
-// 1. Declaring the variable using the keyword: let, var or const
-name = 'Divyanshu';
-console.log(name);
+
+
+// 2. Operators: used to assign/compare values, perform arithmetic operation
+/*
+    * 3. Prompt(): is a built in javaScript functionality that help 
+    *              developer to get the input from a user throgh browser
+*/
+
+fruit = prompt('what is your favourite food')
+console.log(fruit);
