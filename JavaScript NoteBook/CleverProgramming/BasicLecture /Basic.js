631cfd17b904220c44d0fb7ef07c1d4b985827e1
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-07-06 22:06:08 +0530

    Function used in Math -> random, ceil and floor

--- JavaScript NoteBook/CleverProgramming/BasicLecture /Basic.js
+++ JavaScript NoteBook/CleverProgramming/BasicLecture /Basic.js
@@ -1,10 +1,17 @@
-/* 
-    * Printing on the window for i/p taking -> prompt and printing the 
-    * result on the window using alert
-*/
-food = prompt('what is cost of the food in dollars');
-tipPercentage = prompt('percentage of Tip')/100;
-tipAmount = food * tipPercentage;
 
-console.log('the tipAmount is ', tipAmount);
-alert(tipAmount);
+/*
+    * Function which is mostly used in the Coding world:
+    *  1. floor: rounds down the number to the nearest integer
+    *  2. ceil: rounds up the number to the nearest integer
+    *  3. Random: returns a floating-point, pseudo-random 
+    *             number between 0 (inclusive) and 1 (exclusive).
+*/
+
+// Usage
+// In rock-paper and sessior - will need to use the Random function
+
+console.log(Math.floor(3.999999))   //Output: 3
+console.log(Math.floor(-4.7)); // Output: -5
+console.log(Math.ceil(4.3));  // Output: 5
+console.log(Math.ceil(-4.7)); // Output: -4
+console.log(Math.random()); // Output: A random number between 0 and 1, e.g., 0.234567
