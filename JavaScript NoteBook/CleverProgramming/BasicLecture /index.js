d4df2220373590b085b0c80a876814fb707e7f05
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-07-07 21:31:13 +0530

    Object

--- JavaScript NoteBook/CleverProgramming/BasicLecture /index.js
+++ JavaScript NoteBook/CleverProgramming/BasicLecture /index.js
@@ -1,24 +1,52 @@
 /*
-    * Arrays
+    * objects: are a type of variable, quite similar to array but they have key-value pair.
+    * object are created using the squigglies bracket.
+    * the properties of object can be accessed using the dot and bracket notation
 */
 
-    const groceries = ['apple', 'banana'];
-    groceries.push('Mango');
-    // console.log(groceries);     //! [ 'apple', 'banana', 'Mango' ]
+    const person = {
+        name: 'leonardo',
+        shirt: 'white'
+    }
 
-/*
-    * Slice method:
-    * The slice() method is used in both JavaScript arrays and strings to extract a portion 
-    * of the original array or string and return it as a new array or string, without 
-    * modifying the original.
-    * the slice method takes 2 parameter -> start and end. end is not included in the returned
-    * string or array but start is included
-*/
+    // console.log(person);    //! { name: 'leonardo', shirt: 'white' }
+    // console.log(person.name);   //! leonardo
 
-    const array = [10, 20, 30, 40, 50, 60, 70];
-    const slicedArray = array.slice(2, 5);
-    console.log(slicedArray);   // [ 30, 40, 50 ]
+    //* Using bracket  notation
+    // console.log(person['shirt']);       //* white
 
-    console.log(array.length);   // 7 
-    console.log(array.indexOf(44));     // - 1 => means not present 
-    console.log(array.indexOf(40));     // 3
+    // Assign the value to the object:
+    person.phoneNumber = '1-22-333-4444';   // Addint the key value to the object
+    // console.log(person['phoneNumber'])  //1-22-333-4444
+
+
+
+
+    /*
+        Creating a fucntion with 2 param and using the object to store the value
+        of param. Using the template literals for accessing the property and log
+        the result
+    */
+
+        function introduce (name, shirt, assets, liability){
+            const person = {
+                name : name,
+                shirt : shirt,
+                assets : assets,
+                liability: liability,
+                networth: function(){
+                    return this.assets - this.liability
+                }
+            }
+            const intro = `Hi, my name is ${person.name} and my shirt color is ${person['shirt']} and my net worth is ${person.networth()}`
+            return intro;
+        }
+
+        // invoking the function
+
+        console.log(introduce('Divyanshu', 'red', 10000, 5000));
+
+
+
+        
+        // console.log(introduce('Leonardo', 'white'));
