dfcf99a70c5b3e00665334bff7c25bcac88183f3
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-07-07 16:04:38 +0530

    Function in JavaScript

--- JavaScript NoteBook/CleverProgramming/BasicLecture /index.js
+++ JavaScript NoteBook/CleverProgramming/BasicLecture /index.js
@@ -1,15 +1,35 @@
 /*
-    * Variable In javaScript => var, let and const, automatically
-    * Variables declared with var are function-scoped, while let and const are block-scoped.
-    * Variable declared with var can be redeclared whereas not the case with the const and let.
-            * within the same block, we cant declare same variable 2 times using the let keyword but allowed with var
-    * Prefer not to use var becoz of issue in block-scopped.
+    Function:
+    1. Functions are objects in JavaScript and have properties and methods
+    2. Functions are first-class citizens in JavaScript - they can be 
+       assigned to variables, passed as arguments, and returned from other 
+       functions
+    3. 
+
 */
 
-const accountId = 12345;        // Cant be changed
-let accountEmail = 'divyanshu422@gmail.com';
-var accountPassword = '12345';
-accountCity = 'Jaipur';     // Automatically defined -> no used of keyword
+    function greeting (name){
+        console.log('hi ', name,', Nice to meet to you');
+        console.log('hi ' + name + ', Nice to meet to you');
 
-console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+        // Using Template Literals:
+
+        console.log(`hi ${name}, Nice to meet to you`);
+    }
+
+    //  Invoking the function:
+    greeting('Divyanshu');
+
+
+    /*
+        * A function which is named as sum
+        * take 2 argument
+        * does: return the addition of the 2 number. 
+        * the return keyword/statement stops the execution of a function and return a value.
+    */
+
+    function sum (a , b){
+        return (a+b)
+    }
+
+    console.log(sum(10, 20));
