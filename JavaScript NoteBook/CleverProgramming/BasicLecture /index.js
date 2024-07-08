5882902f0abbc168f149ea5441fa5833aa791189
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-07-08 23:07:49 +0530

    Loops

--- JavaScript NoteBook/CleverProgramming/BasicLecture /index.js
+++ JavaScript NoteBook/CleverProgramming/BasicLecture /index.js
@@ -1,84 +1,56 @@
+/*
+    * Loops:
+*/
+        // adding the marks of the student
+        const marks = [70, 45, 76, 53, 90, 80];
+        let sum  = 0;
+        for (const mark of marks){ 
+            sum = sum + mark;
+        }
+
+        // console.log(`the total mark of the student is ${sum}`)
+        // output: the total mark of the student is 414
+
 
 /*
-    * Creating the letter counter: 
-    * Short trick: In javaScript there is a method called length => which gives the total 
-    * letter in the string. so no need to use this code.
+        Letter counter code:
 */
 
-    const howManyLetter = (inputString) => {
-            // Using the for..of loop not the for..in loop
-            let count = 0; 
-            //* console.log(inputString.length); // 26
-            for (let letter of inputString){
-                count++;
+        const howManyLetter = (sentence) => {
+            let count = 0;
+            for (const letter in sentence){
+                count ++;
             }
-            return {count}; //* by using the bracket we are returning the obhect
-    }
+            return count;
+        } 
+
+        // console.log(howManyLetter('hey, can you go to grocery store with me?'))
 
 
-    //console.log(howManyLetter('Hi! I am new to JavaScript'))        //! { count: 26 }
 
-    //* Prompting the user for the input
-    // const input = prompt('Write the string');
-    // console.log(howManyLetter(input));
+        /*
+            For-in loop: used to iterate over the enumerable properities of object.
+            the For..of loop will throw the error on the object.
+        */
 
+            const person = { 
+                name: 'Alice', 
+                age: 25, 
+                city: 'Wonderland' 
+            };
 
-// ---------**************------------**************---------------****************-----------
-    /*
-        * Printing the sum of the array element: using for..of loop
-    */
-
-    const sumArray = (array) =>{
-        let sum = 0;
-        for (let element of array){
-            sum += element;
-        }
-        return {sum};
-    }
-
-    let array = [10,20,30,40,50,60];
-    // console.log(sumArray(array));           //* { sum: 210 }
-
-
-// ---------**************------------**************---------------****************-----------
-    /*
-        * Printing the maximum of the array element: using for..of loop
-    */
-
-    const maxArray = (array) => {
-        let max = array[0];
-        for (let element of array){
-            if (element > max){
-                max = element;
+            for (let key in person) {
+              console.log(key); // logs 'name', 'age', 'city'
+              console.log(person[key]); // logs 'Alice', 25, 'Wonderland'
             }
-        }
-        return {max};
-    }
 
-    // console.log(maxArray([10, 7, 88, 90, 14, 55]));     //! { max: 90 }
+        /*
+            Using the for..in loop on the array => not recomended
+            it will log the index
+        */
+            const array = ['apple', 'banana', 'cherry'];
 
-// --------**************------------**************---------------****************-----------
-/*
-        * Returing the frequency: for example =>
-        *  let the input be 'hahha' 
-        * Then it shall return an object  => {'h':3, 'a': 2}
-*/
-    
-        const letterFrequency = (phrase) => {
-                // Frequency object
-                const frequency = {}
-                // Looping the each letter of string 
-                for (const letter of phrase){
-                    // Checking if the letter is already in the object frequency
-                    if (letter in frequency){
-                        frequency[letter] = frequency[letter] + 1;
-                    }
-                    else{
-                        // Adding the property-value to the object 
-                        frequency[letter] = 1;
-                    }
-                }
-
-                return {frequency};
-        }
-        console.log(letterFrequency('hahha'))           //*  { frequency: { h: 3, a: 2 } }
+            for (let index in array) {
+              console.log(index); // logs '0', '1', '2'
+              console.log(array[index]); // logs 'apple', 'banana', 'cherry'
+            }
