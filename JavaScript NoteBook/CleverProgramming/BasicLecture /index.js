dab538ad1088d39ee871ecdd754e47a69fb6ae98
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-07-08 23:17:49 +0530

    Adding the frequency of occurance in letter

--- JavaScript NoteBook/CleverProgramming/BasicLecture /index.js
+++ JavaScript NoteBook/CleverProgramming/BasicLecture /index.js
@@ -1,56 +1,21 @@
 /*
-    * Loops:
+    * we need to create a function which calculate the frequency of occurance of the
+    * number in the code.
 */
-        // adding the marks of the student
-        const marks = [70, 45, 76, 53, 90, 80];
-        let sum  = 0;
-        for (const mark of marks){ 
-            sum = sum + mark;
+
+    const letterFrequency = (letters) => {
+        // Creating the object which will store the key and value
+        // Key means the letter and value means the frequency
+        let frequency = {};
+        for (const letter of letters){
+            if(letter in frequency){
+                frequency[letter] += 1;
+            }
+            else{
+                frequency[letter] = 1;
+            }
         }
+        return {frequency}
+    }
 
-        // console.log(`the total mark of the student is ${sum}`)
-        // output: the total mark of the student is 414
-
-
-/*
-        Letter counter code:
-*/
-
-        const howManyLetter = (sentence) => {
-            let count = 0;
-            for (const letter in sentence){
-                count ++;
-            }
-            return count;
-        } 
-
-        // console.log(howManyLetter('hey, can you go to grocery store with me?'))
-
-
-
-        /*
-            For-in loop: used to iterate over the enumerable properities of object.
-            the For..of loop will throw the error on the object.
-        */
-
-            const person = { 
-                name: 'Alice', 
-                age: 25, 
-                city: 'Wonderland' 
-            };
-
-            for (let key in person) {
-              console.log(key); // logs 'name', 'age', 'city'
-              console.log(person[key]); // logs 'Alice', 25, 'Wonderland'
-            }
-
-        /*
-            Using the for..in loop on the array => not recomended
-            it will log the index
-        */
-            const array = ['apple', 'banana', 'cherry'];
-
-            for (let index in array) {
-              console.log(index); // logs '0', '1', '2'
-              console.log(array[index]); // logs 'apple', 'banana', 'cherry'
-            }
+    console.log(letterFrequency('hahha'));
