7e34bd6680cf10dd45f5c045b244b24cdbceff35
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-07-09 09:18:28 +0530

    Snapshot 1

---
+++ LoveBabbar/JavaScript/Lecture 1/index.js
@@ -0,0 +1,18 @@
+/*
+        * Html used for skelton of the webpage
+        * CSS: used for the styling the content
+        * JavaScript: to provide the functionality or behaviour of webpage
+         
+        ! what is JavaScript?
+            * It is lightweight programming language or scripting language
+            * Originated by Netscape company in late 1990s.
+            * Originally created as a client side language
+            * To execute the JS code on the browser => we need to have a JS Engine
+                * Example of JS engine: SpiderMonkey (in Morzilla) and V8 (chrome)
+
+        ! What is Node.js?
+            * Node.js uses the V8 JavaScript engine, the same engine that powers Google Chrome, to execute JavaScript code. 
+            * It is an open source & cross-platform JS environment which is used by developer for the server-side code
+        */
+
+
