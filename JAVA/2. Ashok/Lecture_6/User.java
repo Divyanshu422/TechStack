c9b032890030f466005c4dd5afb2b94c5c1a6b1f
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-07-18 16:11:30 +0530

    Lecture 6

---
+++ JAVA/2. Ashok/Lecture_6/User.java
@@ -0,0 +1,19 @@
+/*
+ * Structure of java code:
+                * 1. Package statement [optional] => one package in one file
+                * 2. Import statement [optional]
+                * 3. Interface Section: [optional] mainly used to implement multiple
+                * inheritance
+                * 4. Class Definition: mandatory
+                * 5. Main Method Class: mandatory => where execution is started
+                * -> contains variable [to store tjhe information]
+                * -> contains method [to perform the operation]
+ * 
+ */
+
+/*
+ * Note: there are 2 variable:
+ * method variable
+ * field variable
+ */
+
