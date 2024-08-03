d772d5881f653ff5fe85e6efbbf7ea113a3f3176
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-04 02:58:22 +0530

    Sorting the array: Selection Sort

---
+++ JAVA/2. Ashok/Lecture 21/Program2.java
@@ -0,0 +1,25 @@
+
+/*
+ * Sorting the array using Selection Sort
+ */
+
+import java.util.Arrays;
+
+class Program2 {
+    public static void main(String[] args) {
+        int[] arr = { 64, 34, 25, 12, 22 };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
