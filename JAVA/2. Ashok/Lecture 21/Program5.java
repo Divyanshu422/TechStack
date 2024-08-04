ae633f84fda81bcef2bee83fc33eff2d7db614bd
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-04 12:47:59 +0530

    Unique element in the array

---
+++ JAVA/2. Ashok/Lecture 21/Program5.java
@@ -0,0 +1,22 @@
+/*
+ * Find the Unique element in the array
+ */
+
+public class Program5 {
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 2, 19, 5, 4 };
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j] && i != j) {
+                    count++;
+                }
+            }
+
+            if (count == 0) {
+                System.out.println("The element " + arr[i] + " is unique");
+            }
+        }
+    }
+}
