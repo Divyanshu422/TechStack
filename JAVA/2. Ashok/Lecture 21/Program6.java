71db1c428cd8092287ae9157db0f8aa7e53210b5
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-04 19:30:18 +0530

    Arrays.toString applied on 2d Arrays => resulting into hashCode representation of address

--- JAVA/2. Ashok/Lecture 21/Program6.java
+++ JAVA/2. Ashok/Lecture 21/Program6.java
@@ -1,5 +1,11 @@
 /*
- * 2 dimensional array and printing the data
+ * 2 dimensional array and printing the data:
+ * The Arrays.toString() method in Java is used to convert arrays to their string representation. However, 
+ * when applied to a two-dimensional array, it does not provide a meaningful output. Instead, it treats the 
+ * two-dimensional array as an array of arrays and calls the toString() method on each inner array, 
+ * resulting in a string representation that includes the memory addresses of those arrays rather than their 
+ * contents.
+
  */
 
 import java.util.Arrays;
@@ -17,6 +23,6 @@ class Program6 {
         arr[2][1] = 80;
         arr[2][2] = 90;
 
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr)); // * [[I@7344699f, [I@6b95977, [I@7e9e5f8a] */
     }
 }
